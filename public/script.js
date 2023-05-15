31ab93306e4f2d31d8861218d840bc306cd6eb76
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-05-15 12:30:33 +0200

    Set-up

---
+++ public/script.js
@@ -0,0 +1,134 @@
+// More info button
+
+const infoIcons = document.querySelectorAll(".info-icon")
+
+infoIcons.forEach(infoIcon => {
+    infoIcon.addEventListener("click", (event) => {
+        const moreInfoText = document.getElementById(infoIcon.dataset.szid);
+
+        moreInfoText.classList.toggle("show-more-info")
+    })
+})
+
+
+// Like button
+
+const likeButtons = document.querySelectorAll(".favorite-button")
+
+likeButtons.forEach(likeButton => {
+    likeButton.addEventListener("click", heartAnimation)
+    
+    function heartAnimation () {
+        likeButton.classList.toggle("heart-animation")
+    }
+})
+
+
+// Search button
+
+const searchButtons = document.querySelectorAll(".search-button")
+
+searchButtons.forEach(searchButton => {
+    searchButton.addEventListener("click", searchAnimation)
+    searchButton.addEventListener("animationend", searchAnimation)
+    
+    function searchAnimation () {
+        searchButton.classList.toggle("search-animation")
+    }
+})
+
+
+// Current date
+
+const dateInput = document.getElementById("date-from");
+const dateOutput = document.getElementById("date-till");
+
+// Automaticly change ending date
+
+if(dateInput){
+    dateInput.onchange = () => {
+        dateOutput.value = dateInput.value
+    }
+}
+
+
+// Invalid state form
+
+const inputs = document.querySelectorAll("input, select, textarea")
+
+inputs.forEach(input => {
+    input.addEventListener(
+        "invalid",
+        event => {
+            input.classList.add("invalid")
+        },
+        false
+    );
+});
+
+
+// Map
+
+var map = L.map('map').setView([52.2129919, 5.2793703], 7)
+
+L.tileLayer('https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=6ws7wF90PofV2LnNfOXL', {
+    maxZoom: 19,
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(map)
+
+// Custom marker map
+
+var markerIcon = L.icon({
+    iconUrl: 'assets/img/Icon_search.svg',
+    iconSize:   [15, 19],
+    iconAnchor:  [0, 0],
+})
+
+// Longitude, latitude
+
+var WicherLat = [51.915775, 4.477979]
+var WafaLat = [51.9200524, 4.4660027]
+var KarelLat = [52.3653432, 4.864459999999999]
+var AhmedLat = [51.91511389999999, 4.3975444]
+var ManonLat = [51.9147605, 4.398279]
+
+// Markers
+
+var Wicher = L.marker(WicherLat, {icon: markerIcon})
+var Wafa = L.marker(WafaLat, {icon: markerIcon})
+var Karel = L.marker(KarelLat, {icon: markerIcon})
+var Ahmed = L.marker(AhmedLat, {icon: markerIcon})
+var Manon = L.marker(ManonLat, {icon: markerIcon})
+var markers = L.layerGroup([Wicher, Wafa, Karel, Ahmed, Manon]).addTo(map)
+
+// Link to zone
+
+var searchButton = document.querySelectorAll(".search-button")
+
+searchButton.forEach(button => {
+    button.addEventListener("click", function() {
+        const data = this.dataset.point
+
+        if (data == "Wicher") {
+             map.flyTo(WicherLat, 16)
+        }
+
+        if (data == "Wafa") {
+             map.flyTo(WafaLat, 16)
+        }
+
+        if (data == "Karel") {
+             map.flyTo(KarelLat, 16)
+        }
+
+        if (data == "Ahmed") {
+             map.flyTo(AhmedLat, 16)
+        }
+
+        if (data == "Manon") {
+             map.flyTo(ManonLat, 16)
+        }
+
+
+    })
+});
---
+++ server.js
@@ -0,0 +1,112 @@
+//import express en .env
+import express from "express"
+import dotenv from 'dotenv'
+
+//activeer .env
+dotenv.config()
+
+//maak een nieuwe express app
+const server = express()
+
+//views en public instellen
+server.use(express.static("public"))
+server.set("view engine", "ejs")
+server.set("views", "./views")
+
+//stel afhandeling van formulieren in
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+
+//routes 
+
+//index - get
+
+server.get("/", (request, response) => {
+    let urlSmartzones = `${process.env.API_URL}/smartzones`
+  fetchJson(urlSmartzones).then((data) => {
+    response.render("index", {smartzones: data.smartzones})
+    })
+  })
+
+//book - get
+
+server.get("/book", (request, response) => {
+  let urlSmartzones = `${process.env.API_URL}/smartzones`
+  fetchJson(urlSmartzones).then((smartzones) => {
+    let id = request.query.id
+    let url = `${process.env.API_URL}/reservations?id=${id}`
+    let time = request.query.time
+    fetchJson(url).then((reservations) => {
+      let data = {smartzones: smartzones, reservations: reservations}
+      response.render("book", {smartzones: data.smartzones.smartzones, selectedSmartzoneId: id, time: time})
+    })
+  })
+})
+
+//book - post
+
+server.post('/book', (request, response) => {
+  const url = `${process.env.API_URL}/reservations`
+  request.body.timeStart = request.body.dateStart + 'T' + request.body.timeStart + ':00Z';
+  request.body.timeEnd = request.body.dateEnd + 'T' + request.body.timeEnd + ':00Z';
+
+  postJson(url, request.body).then((data) => {
+    let newReservation = { ... request.body }
+
+    if (data.data.id.length > 0) {
+      response.redirect('/book?reservationPosted=true') 
+    } else {
+      const errorMessage = data.message + "Some fields are not filled in (correctly)."
+      const newData = { error: errorMessage, values: newReservation }
+      
+      let urlSmartzones = `${process.env.API_URL}/smartzones`
+        fetchJson(urlSmartzones).then((smartzones) => {
+          let id = request.query.id
+          let time = request.query.time
+          let url = `${process.env.API_URL}/reservations?id=${id}`
+          fetchJson(url).then((reservations) => {
+            let data = {smartzones: smartzones, reservations: reservations, newData}
+            response.render("book", {smartzones: data.smartzones.smartzones, selectedSmartzoneId: id, time: time})
+          })
+        })
+    }
+  })
+})
+
+
+//poortnummer instellen
+server.set("port", 8000)
+
+//start de server
+server.listen(server.get("port"), () => {
+  console.log(`Application started on http://localhost:${server.get("port")}`)
+})
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+
+/**
+ * postJson() is a wrapper for the experimental node fetch api. It fetches the url
+ * passed as a parameter using the POST method and the value from the body paramater
+ * as a payload. It returns the response body parsed through json.
+ * @param {*} url the api endpoint to address
+ * @param {*} body the payload to send along
+ * @returns the json response from the api endpoint
+ */
+export async function postJson(url, body) {
+  return await fetch(url, {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .catch((error) => error)
+}
