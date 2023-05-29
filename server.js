a1dde8fb1c4cb50977030e688ce5f02742d8a311
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-05-29 19:22:19 +0200

    refactor

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { Server } from 'socket.io'
 import { createServer, request } from 'http'
 import express, { response } from 'express'
 import dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from "openai"
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
@@ -26,9 +26,9 @@ const ioServer = new Server(http, {
 
 
 //views en public instellen
-server.use(express.static(path.resolve("public")))
-server.set("view engine", "ejs")
-server.set("views", "./views")
+server.use(express.static(path.resolve('public')))
+server.set('view engine', 'ejs')
+server.set('views', './views')
 
 //stel afhandeling van formulieren in
 server.use(express.json())
@@ -38,25 +38,25 @@ server.use(express.urlencoded({ extended: true }))
 
 //index - get
 
-server.get("/", (request, response) => {
+server.get('/', (request, response) => {
   let urlSmartzones = `${process.env.API_URL}/smartzones`
   fetchJson(urlSmartzones).then((data) => {
-    response.render("index", { smartzones: data.smartzones })
+    response.render('index', { smartzones: data.smartzones })
   })
 })
 
 //manage - get
 
-server.get("/manage", (request, response) => {
+server.get('/manage', (request, response) => {
   let urlSmartzones = `${process.env.API_URL}/smartzones`
   fetchJson(urlSmartzones).then((data) => {
-    response.render("manage", { smartzones: data.smartzones })
+    response.render('manage', { smartzones: data.smartzones })
   })
 })
 
 //book - get
 
-server.get("/book", (request, response) => {
+server.get('/book', (request, response) => {
   let urlSmartzones = `${process.env.API_URL}/smartzones`
   fetchJson(urlSmartzones).then((smartzones) => {
     let id = request.query.id
@@ -64,7 +64,7 @@ server.get("/book", (request, response) => {
     let time = request.query.time
     fetchJson(url).then((reservations) => {
       let data = { smartzones: smartzones, reservations: reservations }
-      response.render("book", { smartzones: data.smartzones.smartzones, selectedSmartzoneId: id, time: time })
+      response.render('book', { smartzones: data.smartzones.smartzones, selectedSmartzoneId: id, time: time })
     })
   })
 })
@@ -82,7 +82,7 @@ server.post('/book', (request, response) => {
     if (data.data.id.length > 0) {
       response.redirect('/book?reservationPosted=true')
     } else {
-      const errorMessage = data.message + "Some fields are not filled in (correctly)."
+      const errorMessage = data.message + 'Some fields are not filled in (correctly).'
       const newData = { error: errorMessage, values: newReservation }
 
       let urlSmartzones = `${process.env.API_URL}/smartzones`
@@ -92,7 +92,7 @@ server.post('/book', (request, response) => {
         let url = `${process.env.API_URL}/reservations?id=${id}`
         fetchJson(url).then((reservations) => {
           let data = { smartzones: smartzones, reservations: reservations, newData }
-          response.render("book", { smartzones: data.smartzones.smartzones, selectedSmartzoneId: id, time: time })
+          response.render('book', { smartzones: data.smartzones.smartzones, selectedSmartzoneId: id, time: time })
         })
       })
     }
@@ -112,19 +112,19 @@ ioServer.on('connection', (client) => { //als er connectie gemaakt word
       while (conversationHistory.length > historySize) {   //als history vol zit
         conversationHistory.shift()  //history legen
       }
-      conversationHistory.push({ role: "user", content: message })  //zet binnenkoment message in history
+      conversationHistory.push({ role: 'user', content: message })  //zet binnenkoment message in history
       const completion = await openai.createChatCompletion({   //ai aanroepen
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         messages: conversationHistory,  //history aan ai vragen
       })
       const response = completion.data.choices[0].message.content //ai data meegeven aan response
-      conversationHistory.push({ role: "assistant", content: response })  //ai response naar history
+      conversationHistory.push({ role: 'assistant', content: response })  //ai response naar history
       console.log(response)
       client.emit('message', response)  //ai message naar client sturen
       callback()
     } catch (error) { //als er een error is
       console.error(error.response.data)
-      callback("Error: Unable to connect to the chatbot") //error naar client sturen
+      callback('Error: Unable to connect to the chatbot') //error naar client sturen
     }
   })
 
@@ -138,11 +138,11 @@ ioServer.on('connection', (client) => { //als er connectie gemaakt word
 
 
 //poortnummer instellen
-server.set("port", process.env.PORT || 8000)
+server.set('port', process.env.PORT || 8000)
 
 //start de server
-http.listen(server.get("port"), "0.0.0.0", () => {
-  console.log(`Application started on http://localhost:${server.get("port")}`)
+http.listen(server.get('port'), '0.0.0.0', () => {
+  console.log(`Application started on http://localhost:${server.get('port')}`)
 })
 
 /**
@@ -159,9 +159,9 @@ async function fetchJson(url) {
 
 export async function postJson(url, body) {
   return await fetch(url, {
-    method: "post",
+    method: 'post',
     body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   })
     .then((response) => response.json())
     .catch((error) => error)
