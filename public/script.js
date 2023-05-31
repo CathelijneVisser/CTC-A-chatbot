2299672d272372d23b5c3e00311c3717fdedb7be
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-05-31 09:28:33 +0200

    Map fix

--- public/script.js
+++ public/script.js
@@ -88,7 +88,7 @@ if (checkMap) {
 
   L.tileLayer("https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=6ws7wF90PofV2LnNfOXL", {
     maxZoom: 19,
-    attribution: "<a href="https://www.maptiler.com/copyright/" target="_blank">&copy MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy OpenStreetMap contributors</a>",
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy OpenStreetMap contributors</a>',
   }).addTo(map)
 
   // Custom marker map
